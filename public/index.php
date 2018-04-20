b719d62e328ddaab179582c22992aa45e395ec8e
Kevin Musters <kevinmusters@hotmail.com>
2018-04-20 09:39:59 +0200

    Add Main site style
    Added main site, styled menu.

--- public/index.php
+++ public/index.php
@@ -1,10 +1,31 @@
-<?php require(realpath(__DIR__) . '/templates/header.php'); ?>
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en" class="dashboard">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <title>Fifa</title>
+</head>
+<body>
+    <?php require('templates/header.php'); ?>
+    <div class="main">
+        <div class="header">
+            <h2>FIFA Dev Edition</h2>
+            <h3>Wedstrijden</h3>
+        </div>
+        <div class="content">
 
-    <div class="main-content">
+        </div>
+        <div class="footer">
 
-        <!-- site's main content goes here -->
-
-
+        </div>
     </div>
-
-<?php require(realpath(__DIR__) . '/templates/footer.php');
+    <?php require('templates/footer.php'); ?>
+</body>
+</html>
--- public/templates/header.php
+++ public/templates/header.php
@@ -1,12 +1,46 @@
-<!--This header is just a suggestion. Do whatever you want with it!-->
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Project Fifa</title>
-    <!-- you can link bootstrap if you want.   -->
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+<div id="header">
+    <div class="container">
+        <div>
+            <h1>Fifa</h1>
+        </div>
+        <?php
+        if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true && isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true) {
+            echo '<p>Je bent ingelogt als ADMIN</p>';
+        } elseif (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true && isset($_SESSION['isCaptain']) && $_SESSION['isCaptain'] == true) {
+            echo '<p>Je bent ingelogt als TEAM CAPTAIN</p>';
+        } elseif (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true) {
+            echo '<p>Je bent ingelogt als SPELER</p>';
+        }
+        else{
 
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
+        }
+        ?>
+        <nav>
+            <a href="/index.php"><i class="fas fa-trophy"></i>Wedstrijden</a>
+            <a href=""><i class="fas fa-star"></i>Resultaten</a>
+            <?php
+            if(!isset($_SESSION['isLoggedIn']) || $_SESSION['isLoggedIn'] == false) {
+            ?>
+            <a href=""><i class="fas fa-sign-in-alt"></i>Login</a>
+            <?php } ?>
+            <?php if((isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true)
+                || (isset($_SESSION['isCaptain']) && $_SESSION['isCaptain'] == true)){ ?>
+
+                <a href=""><i class="fas fa-users"></i>Team aanmaken</a>
+
+            <?php } ?>
+            <?php if((isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true)
+                || (isset($_SESSION['isCaptain']) && $_SESSION['isCaptain'] == true)){ ?>
+
+                <a href=""><i class="fas fa-sort-numeric-up"></i>Standen invoeren</a>
+
+            <?php }
+            if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true) {
+            ?>
+
+                <a href="/app/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+
+            <?php } ?>
+        </nav>
+    </div>
+</div>
