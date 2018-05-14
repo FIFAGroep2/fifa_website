35d23eaf54f4191bacd044076df749304e60b165
Abdulla Donmez <abdulladonmez@hotmail.com>
2018-05-14 10:54:03 +0200

    added 2 folders
    assets/css/style.css
    and templates/footer.php
    and templates/header.php
    
    has been added

---
+++ public/templates/footer.php
@@ -0,0 +1,87 @@
+<div id="footer">
+    <div class="container">
+        <h2>Poulestanden</h2>
+        <h4>Poule 1</h4>
+        <table>
+            <tr>
+                <th>Nr</th>
+                <th>Team</th>
+                <th>Score</th>
+            </tr>
+            <?php
+            header('Content-Type: text/html; charset=ISO-8859-1');
+            $teams = $server->prepare("SELECT * FROM `tbl_teams` ORDER BY `score` DESC");
+            $teams->execute();
+            $teams = $teams->fetchAll();
+
+            $rank = 1;
+            foreach ($teams as $team){
+                $poule = $team['poule_id'];
+                if ($poule == 1) {
+                    echo '<tr>';
+                    echo '<td>' . $rank . '</td>';
+                    echo '<td class="teamname name">' . $team['name'] . '</td>';
+                    echo '<td>' . $team['score'] . '</td>';
+                    echo '</tr>';
+                    $rank++;
+                }
+            }
+            ?>
+        </table>
+
+        <h4>Poule 2</h4>
+        <table>
+            <tr>
+                <th>Nr</th>
+                <th>Team</th>
+                <th>Score</th>
+            </tr>
+            <?php
+            $rank = 1;
+            foreach ($teams as $team){
+                $poule = $team['poule_id'];
+                if ($poule == 2) {
+                    echo '<tr>';
+                    echo '<td>' . $rank . '</td>';
+                    echo '<td class="teamname name">' . $team['name'] . '</td>';
+                    echo '<td>' . $team['score'] . '</td>';
+                    echo '</tr>';
+                    $rank++;
+                }
+            }
+            ?>
+        </table>
+
+        <h4 class="header-userrank">User Ranking</h4>
+        <table>
+            <tr>
+                <th>Nr</th>
+                <th>Naam</th>
+                <th>Team</th>
+                <th>Score</th>
+            </tr>
+            <tr>
+                <?php
+                $players = $server->prepare("SELECT * FROM `tbl_players` ORDER BY `score` DESC LIMIT 10");
+                $players->execute();
+                $players = $players->fetchAll();
+
+                $rank = 1;
+                foreach ($players as $player) {
+                    $teamid = $player['team_id'];
+                    $team = $server->prepare("SELECT * FROM `tbl_teams` WHERE `id` = '$teamid'");
+                    $team->execute();
+                    $team = $team->fetch(PDO::FETCH_ASSOC);
+                    echo '<tr>';
+                    echo '<td>' . $rank . '</td>';
+                    echo '<td class="playername name">' . $player['first_name'] . ' ' . $player['last_name'] . '</td>';
+                    echo '<td>' . $team['name'] . '</td>';
+                    echo '<td>' . $player['score'] . '</td>';
+                    echo '</tr>';
+                    $rank++;
+                }
+                ?>
+            </tr>
+        </table>
+    </div>
+</div>
---
+++ public/templates/header.php
@@ -0,0 +1,48 @@
+<div id="header">
+    <div class="container">
+        <div>
+            <h1>Fifa</h1>
+        </div>
+        <?php
+        $_SESSION['isAdmin'] = true;
+        $_SESSION['isLoggedIn'] = true;
+        if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true && isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true) {
+            echo '<p>Je bent ingelogt als ADMIN</p>';
+        } elseif (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true && isset($_SESSION['isCaptain']) && $_SESSION['isCaptain'] == true) {
+            echo '<p>Je bent ingelogt als TEAM CAPTAIN</p>';
+        } elseif (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true) {
+            echo '<p>Je bent ingelogt als SPELER</p>';
+        }
+        else{
+
+        }
+        ?>
+        <nav>
+            <a href="../index.php"><i class="fas fa-trophy"></i>Wedstrijden</a>
+            <a href=""><i class="fas fa-star"></i>Resultaten</a>
+            <?php
+            if(!isset($_SESSION['isLoggedIn']) || $_SESSION['isLoggedIn'] == false) {
+                ?>
+                <a href=""><i class="fas fa-sign-in-alt"></i>Login</a>
+            <?php } ?>
+            <?php if((isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true)
+                || (isset($_SESSION['isCaptain']) && $_SESSION['isCaptain'] == true)){ ?>
+
+                <a href="../public/create_team.php"><i class="fas fa-users"></i>Team aanmaken</a>
+
+            <?php } ?>
+            <?php if((isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true)
+                || (isset($_SESSION['isCaptain']) && $_SESSION['isCaptain'] == true)){ ?>
+
+                <a href=""><i class="fas fa-sort-numeric-up"></i>Standen invoeren</a>
+
+            <?php }
+            if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == true) {
+                ?>
+
+                <a href="/app/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+
+            <?php } ?>
+        </nav>
+    </div>
+</div>
