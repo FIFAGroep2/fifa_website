17c7dcb840ea69699af416711698058bf101dad7
Kevin Musters <kevinmusters@hotmail.com>
2018-05-31 16:52:46 +0200

    add ignore stuff

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!doctype html>
+<html lang="en" class="dashboard">
+<head>
+    <link rel="icon" href="public/icon.ico" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link type="text/css" rel="stylesheet" href="public/assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js/dist/jquery.bracket.min.js"></script>
+
+    <link type="text/css" href="js/jquery-ui.min.css">
+    <link type="text/css" href="js/jquery-ui.structure.min.css">
+    <link type="text/css" href="js/jquery-ui.theme.min.css">
+    <link type="text/css" href="js/dist/jquery.bracket.min.css" rel="stylesheet">
+
+    <title>Finales</title>
+</head>
+<body>
+<p>
+    <?php
+    echo "Redirecting...";
+    ?>
+</p>
+<?php
+header('location: public/index.php');
+exit;
+?>
+</body>
+</html>
