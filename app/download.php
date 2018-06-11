c9454ae64fda6728cb95ae7257e4996dbebe0416
Kevin Musters <kevinmusters@hotmail.com>
2018-06-11 09:17:55 +0200

    Update application download
    Updated download page for application!

--- app/download.php
+++ app/download.php
@@ -1,8 +1,14 @@
 <?php
-$file_url = '../files/README.txt';
-header('Content-Type: text/plain');
-header("Content-Transfer-Encoding: Binary");
-header("Content-disposition: attachment; filename=" . basename($file_url));
-readfile($file_url);
-exit;
+$file_url = '../files/Fifa_v2.rar';
+if (file_exists($file_url) && is_readable($file_url)) {
+    $size = filesize($file_url);
+    header('Content-Type: application/x-rar-compressed, application/octet-stream');
+    header('Content-Length: ' . $size);
+    header('Content-Transfer-Encoding: binary');
+    header("Content-disposition: attachment; filename=" . basename($file_url));
+    readfile($file_url);
+    exit;
+} else {
+    header('location: ../public/index.php?message=Bestand niet gevonden!');
+}
 ?>
